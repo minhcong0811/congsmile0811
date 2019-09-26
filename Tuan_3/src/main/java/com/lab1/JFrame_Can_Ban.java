63cc9510ee4eea0a40ac432feeeac5b5e3fac65b
phaminhcong0811 <55102506+phaminhong0811@users.noreply.github.com>
2019-09-26 20:23:14 +0700

    update

---
+++ Tuan_3/src/main/java/com/lab1/JFrame_Can_Ban.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author neroooo
+ */
+public class JFrame_Can_Ban extends JFrame {
+    public JFrame_Can_Ban(){
+        setVisible(true);
+        
+    }
+    
+    public static void main(String[] args) {
+        new JFrame_Can_Ban();
+    }
+}
